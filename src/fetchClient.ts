9d6c2debb9e2090c6af38ea61f31396c665b175d
Eugene Sherepa <evgenijserepa@gmail.com>
2023-08-27 19:02:17 +0300

    link fix

--- src/fetchClient.ts
+++ src/fetchClient.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:8080/api";
+export const BASE_URL = "https://media-service-api.onrender.com";
 
 type RequestMethod = 'GET' | 'POST' | 'DELETE' | 'PATCH';
 
